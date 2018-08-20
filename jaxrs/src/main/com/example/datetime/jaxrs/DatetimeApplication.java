e942ba6ede1f2afd4d4b03fc6ef0c91e2c4f2cca
alvinsim <sim.alvin@gmail.com>
2018-08-21 00:27:51 +1000

    added dependencies to jersey libraries

---
+++ jaxrs/src/main/com/example/datetime/jaxrs/DatetimeApplication.java
@@ -0,0 +1,9 @@
+package com.example.datetime.jaxrs;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class DatetimeApplication extends Application {
+
+}

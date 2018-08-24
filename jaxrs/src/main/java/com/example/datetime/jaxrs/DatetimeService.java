faaf471ba8a1c697a88c1903dd0f315796166d57
alvinsim <sim.alvin@gmail.com>
2018-08-24 23:44:24 +1000

    more test for fail scenario and 1 successful scenario. plus completed the countdays() functionality

--- jaxrs/src/main/java/com/example/datetime/jaxrs/DatetimeService.java
+++ jaxrs/src/main/java/com/example/datetime/jaxrs/DatetimeService.java
@@ -1,10 +1,73 @@
 package com.example.datetime.jaxrs;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Path("datetime")
+@Path("/datetime")
 public class DatetimeService {
 
+    private final Logger logger = LoggerFactory.getLogger(DatetimeService.class);
+    final static String ERROR_MESSAGE_PARSE_DATE = "Error: Problem parsing date %s";
 
+    @GET
+    @Path("/echo/{datetime}")
+    public Response echo(@PathParam("datetime") String datetime) {
+        return Response
+                .status(Response.Status.OK)
+                .entity(String.format("datetime: %s", datetime))
+                .build();
+    }
 
+    @GET
+    @Path("/countDays")
+    public Response calculateDays(@QueryParam("from") String from,
+                                  @QueryParam("to") String to) {
+        LocalDate fromDate = null;
+        LocalDate toDate = null;
+
+        try {
+            fromDate = parseDate(from);
+        } catch (DateTimeException e) {
+            logger.error(String.format(ERROR_MESSAGE_PARSE_DATE, from), e);
+            return appendMessageToResponseWIthStatusOK(String.format(ERROR_MESSAGE_PARSE_DATE, from));
+        }
+        try {
+            toDate = parseDate(to);
+        } catch (DateTimeException e) {
+            logger.error(String.format(ERROR_MESSAGE_PARSE_DATE, to), e);
+            return appendMessageToResponseWIthStatusOK(String.format(ERROR_MESSAGE_PARSE_DATE, to));
+        }
+
+        long days = ChronoUnit.DAYS.between(fromDate, toDate);
+        return Response
+                .status(Response.Status.OK)
+                .entity(String.format("Result: %d", days))
+                .build();
+    }
+
+    private LocalDate parseDate(String date) throws DateTimeException {
+        if (StringUtils.isBlank(date)) {
+            throw new DateTimeException("Cannot parse a blank date.");
+        }
+        return LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
+
+    private Response appendMessageToResponseWIthStatusOK(String message) {
+        return Response.status(Status.OK).entity(message).build();
+    }
+
+    private Response appendMessageWithStatusToResponse(Status status, String message) {
+        return Response.status(status).entity(message).build();
+    }
 }
--- jaxrs/src/test/java/com/example/datetime/jaxrs/DatetimeServiceTest.java
+++ jaxrs/src/test/java/com/example/datetime/jaxrs/DatetimeServiceTest.java
@@ -12,11 +12,55 @@ public class DatetimeServiceTest extends JerseyTest {
     protected Application configure() {
         return new ResourceConfig(DatetimeService.class);
     }
-    
+
     @Test
-    public void echoDatetimeTest() {
-        String response = target("datetime/2018-08-23").request().get(String.class);
+    public void shouldReturnSameParameterValue() {
+        String response = target("/datetime/echo/2018-08-23").request().get(String.class);
         Assert.assertTrue("datetime: 2018-08-23".equals(response));
     }
 
+    @Test
+    public void shouldReturnCalculatedDaysFromTwoDates() {
+        String response = target("/datetime/countDays")
+                .queryParam("from", "2018-08-23")
+                .queryParam("to", "2018-08-24")
+                .request()
+                .get(String.class);
+        Assert.assertEquals(response, "Result: " + 1);
+    }
+
+    @Test
+    public void shouldReturnWithErrorMessageForInvalidDateWhenCalculatingDaysFromTwoDates() {
+        String errorFromDate = "2018-089";
+        String responseWithErrorFromDate = target("datetime/countDays")
+                .queryParam("from", errorFromDate)
+                .queryParam("to", "2018-08-10")
+                .request()
+                .get(String.class);
+        Assert.assertEquals(responseWithErrorFromDate, String.format(DatetimeService.ERROR_MESSAGE_PARSE_DATE, errorFromDate));
+
+        String errorToDate = "abc";
+        String responseWithErrorToDate = target("datetime/countDays")
+                .queryParam("from", "2018-08-10")
+                .queryParam("to", errorToDate)
+                .request()
+                .get(String.class);
+        Assert.assertEquals(responseWithErrorToDate, String.format(DatetimeService.ERROR_MESSAGE_PARSE_DATE, errorToDate));
+
+        String responseWithBlankFromDate = target("datetime/countDays")
+                .queryParam("from", "")
+                .queryParam("to", "2018-08-08")
+                .request()
+                .get(String.class);
+        Assert.assertEquals(responseWithErrorToDate, String.format(DatetimeService.ERROR_MESSAGE_PARSE_DATE, errorToDate));
+
+        String responseWithNoFromDate = target("datetime/countDays")
+                .queryParam("to", "2018-08-08")
+                .request()
+                .get(String.class);
+        Assert.assertEquals(responseWithErrorToDate, String.format(DatetimeService.ERROR_MESSAGE_PARSE_DATE, errorToDate));
+
+        String responseWithNoDates = target("datetime/countDays").request().get(String.class);
+        Assert.assertEquals(responseWithErrorToDate, String.format(DatetimeService.ERROR_MESSAGE_PARSE_DATE, errorToDate));
+    }
 }
